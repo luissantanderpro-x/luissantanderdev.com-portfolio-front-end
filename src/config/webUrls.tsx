f088a74653ff9de57551b31f2593fe506935c0ce
luissantanderdev <luissantanderdev@gmail.com>
2025-01-16 14:22:21 -0800

    changed weburl for resume.

--- src/config/webUrls.tsx
+++ src/config/webUrls.tsx
@@ -4,7 +4,7 @@
 export const WEB_URLS = {
     linkedin: 'https://www.linkedin.com/in/luissantanderpro/',
     github: 'https://github.com/luissantanderpro-x', 
-    google_resume: 'https://drive.google.com/uc?export=download&id=1UQgFipGU-zMkeRWnk5E4E7wxr_uXbRp1',
+    google_resume: 'https://docs.google.com/document/d/1QRL1i8C5sR-i2c2kh_ibmjIT-zwbhatZ/export?format=docx',
     ipify_url: 'https://api.ipify.org?format=json'
 }; 
 
